0e985e70312705e15b6fc5c7cc6aaa1ce30eb9df
Lubov-kost <futoke@gmail.com>
2023-11-12 01:06:04 +0300

    task 2

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #task 1
 
-RED = '\u001b[41m'
-BLUE = '\u001b[44m'
-WHITE = '\u001b[47m'
-END = '\u001b[0m'
+# RED = '\u001b[41m'
+# BLUE = '\u001b[44m'
+# WHITE = '\u001b[47m'
+# END = '\u001b[0m'
 
 
-'''for i in range(6):
-    if i<2:
-        print(f'{RED}{"  "*14}{END}')
-    if 2<=i<4:
-        print(f'{WHITE}{"  "*14}{END}')
-    if i>=4:
-        print(f'{BLUE}{"  "*14}{END}')'''
+# for i in range(6):
+#     if i<2:
+#         print(f'{RED}{"  "*14}{END}')
+#     if 2<=i<4:
+#         print(f'{WHITE}{"  "*14}{END}')
+#     if i>=4:
+#         print(f'{BLUE}{"  "*14}{END}')
 
 #task 2
 
 BLACK = '\u001b[30m'
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
+for i in range(1,6):
+    if i == 3:
+        print(f'{WHITE}{"  "*2}{BLACK}{"  "}{WHITE}{"  "*3}{BLACK}{"  "}{WHITE}{"  "*2}{END}')
+    elif i%2==1:
+        print(f'{BLACK}{"  "}{WHITE}{"  "*3}{BLACK}{"  "}{WHITE}{"  "*3}{BLACK}{"  "}{END}')
+    else:
+        print(f'{WHITE}{"  "}{BLACK}{"  "}{WHITE}{"  "}{BLACK}{"  "}{WHITE}{"  "}{BLACK}{"  "}{WHITE}{"  "}{BLACK}{"  "}{WHITE}{"  "}{END}')
+
 
 #task 3
 
@@ -46,16 +54,16 @@ END = '\u001b[0m'
 
 #task 4
 
-k0,k1=0,0
-with open('sequence.txt') as f:
-    s = []
-    for i in f:
-        s.append(float(i))
+# k0,k1=0,0
+# with open('sequence.txt') as f:
+#     s = []
+#     for i in f:
+#         s.append(float(i))
     
-    for i in range(len(s)):
-        if i%2==0:
-            k0 += abs(s[i])
-        else:
-            k1 += abs(s[i])
-    print(k0,k1)
+#     for i in range(len(s)):
+#         if i%2==0:
+#             k0 += abs(s[i])
+#         else:
+#             k1 += abs(s[i])
+#     print(k0,k1)
 
