daa6f56d25f158868b61bc814c3063889b3e37d0
Lubov-kost <futoke@gmail.com>
2023-11-12 00:30:42 +0300

    task 3,4

--- main.py
+++ main.py
@@ -1,63 +1,61 @@
+#task 1
+
 RED = '\u001b[41m'
 BLUE = '\u001b[44m'
 WHITE = '\u001b[47m'
 END = '\u001b[0m'
 
-'''for i in range(6):
-    if i < 3:
-        print(f'{BLUE}{"  " * (2 * i + 2)}{WHITE}{"  " * (14 - 2 * i)}{END}')
-    else:
-        print(f'{BLUE}{"  " * (12 - 2 * i)}{RED}{"  " * (4 + 2 * i)}{END}')'''
 
-for i in range(6):
+'''for i in range(6):
     if i<2:
         print(f'{RED}{"  "*14}{END}')
     if 2<=i<4:
         print(f'{WHITE}{"  "*14}{END}')
     if i>=4:
-        print(f'{BLUE}{"  "*14}{END}')
+        print(f'{BLUE}{"  "*14}{END}')'''
 
+#task 2
 
-# plot_list = [[0 for i in range(10)] for i in range(10)]
-# result = [0 for i in range(10)]
+BLACK = '\u001b[30m'
+WHITE = '\u001b[47m'
+END = '\u001b[0m'
 
+#task 3
+
+# p = [[0 for i in range(10)] for i in range(10)]
+# r = [0 for i in range(10)]
 # for i in range(10):
-#     result[i] = i ** 3
-
-# step = round(abs(result[0] - result[9]) / 9, 2)
-# print(step)
-
-# for i in range(10):
-#     for j in range(10):
-#         if j == 0:
-#             plot_list[i][j] = step * (8-i) + step
+#     r[i] = i*2
+# r = r[::-1]
 
 # for i in range(9):
-#     for j in range(10):
-#         if abs(plot_list[i][0] - result[9 - j]) < step:
-#             for k in range(9):
-#                 if 8 - k == j:
-#                     plot_list[i][k+1] = 1
+#     for j in range(9):
+#         p[i][8-i] = '*'
 
 # for i in range(9):
 #     line = ''
 #     for j in range(10):
 #         if j == 0:
-#             line += '\t' + str(int(plot_list[i][j])) + '\t'
-#         if plot_list[i][j] == 0:
+#             line += '\t' + str(r[i]) + '\t'
+#         if p[i][j] == 0:
 #             line += '--'
-#         if plot_list[i][j] == 1:
+#         if p[i][j] == "*":
 #             line += '!!'
 #     print(line)
-# print('\t0\t1 2 3 4 5 6 7 8 9')
+# print('\t0\t1 2 3 4 5 6 7 8 9 10')
 
-# for i in range(10):
-#     #print(plot_list[i])
-#     pass
+#task 4
 
-# file = open('sequence.txt', 'r')
-# list = []
-# for number in file:
-#     list.append(float(number))
-# file.close()
-# print(list)
+k0,k1=0,0
+with open('sequence.txt') as f:
+    s = []
+    for i in f:
+        s.append(float(i))
+    
+    for i in range(len(s)):
+        if i%2==0:
+            k0 += abs(s[i])
+        else:
+            k1 += abs(s[i])
+    print(k0,k1)
+
